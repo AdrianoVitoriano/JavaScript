53716e043c218b3309f506c1244818617701cca4
Adriano V. da Silva <adrianovitorianosilva@gmail.com>
2024-04-02 19:16:50 -0300

    Alterações no CRUD

--- CursoNode/FSModule/Create.js
+++ CursoNode/FSModule/Create.js
@@ -1,39 +1,72 @@
-import fs from "fs";
 import { createFile, readFile, promptData } from "./CRUD.js";
 import PromptSync from "prompt-sync";
-import { type } from "os";
-import {showPosts} from "./view.js"
+import { showPosts, searchPost, options } from "./view.js";
 const prompt = PromptSync();
-
-const nomeArquivo = prompt("Digite o nome do arquivo: ") + ".json";
 const crud = {
   post: [],
-  search(arquivo, id, title, content, date){
-    this.read(arquivo)
-    
-
+  search(arquivo) {
+    this.read(arquivo);
+    const search = searchPost(this.post);
+    return search;
   },
-  view(arquivo){
-    this.read(arquivo)
-    for(let item of this.post){
-      showPosts(item.id, item.title,item.content, item.date)
+  view(arquivo) {
+    this.read(arquivo);
+    if (this.post[0] === undefined) {
+      showPosts(this.post.id, this.post.title, this.post.content, this.post.date, 70);
+    } else {
+      for (let item of this.post) {
+        showPosts(item.id, item.title, item.content, item.date, 70);
+      }
     }
   },
   read(arquivo) {
     this.post = readFile(arquivo);
     return this.post;
   },
-  create(arquivo, data) {
-    if(this.post[0] === undefined){
-        this.post = [this.post,data]
-    }else{
-        this.post.push(data)
+  create(arquivo) {
+    this.read(arquivo);
+    if (this.post[0] === undefined) {
+      this.post = [this.post, promptData()];
+    } else {
+      this.post.push(promptData());
     }
-    
     createFile(arquivo, this.post);
   },
 };
-crud.read(nomeArquivo);
-crud.view(nomeArquivo)
-crud.create(nomeArquivo, promptData());
+async function main() {
+  let nomeArquivo = prompt("Digite o nome do arquivo: ") + ".json";
+  console.log("\n");
+  let option;
+  do {
+    option = options("main");
+    switch (option) {
+      case 1:
+        crud.create(nomeArquivo);
+        break;
+      case 2:
+        crud.view(nomeArquivo);
+        break;
+      case 3:
+        crud.search(nomeArquivo);
+        break;
+      case 4:
+        nomeArquivo = prompt("Digite o nome do arquivo: ") + ".json";
+        crud.read(nomeArquivo);
+        break;
+      case 5:
+        console.log("Saindo");
+        console.log("\n");
+        break;
+      default:
+        console.log("Erro... Saindo");
+        console.log("\n");
+    }
+  } while (option !== 5);
+}
 
+main();
+
+//crud.read(nomeArquivo)
+//crud.search(nomeArquivo)
+//crud.view(nomeArquivo)
+//crud.create(nomeArquivo, promptData());
--- CursoNode/FSModule/view.js
+++ CursoNode/FSModule/view.js
@@ -1,29 +1,154 @@
-import nock from "nock";
 import PromptSync from "prompt-sync";
-const prompt = PromptSync()
+const prompt = PromptSync();
 
-export function  showPosts(idPost, title, content, date){
-    let lengthWindow = 70
-    let underline = `|${'_'.repeat(lengthWindow-2)}|`
-    console.log(underline)
-    lineBreak('Id Post:',idPost,lengthWindow)
-    lineBreak('Title:',title,lengthWindow)
-    lineBreak('Content:',content,lengthWindow)
-    lineBreak('Date:',String(date),lengthWindow)
-    console.log(underline)
+export function showPosts(idPost, title, content, date, lengthWindow) {
+  let underline = `|${"_".repeat(lengthWindow - 2)}|`;
+  console.log(underline);
+  lineBreak("Id Post:", idPost, lengthWindow);
+  lineBreak("Title:", title, lengthWindow);
+  lineBreak("Content:", content, lengthWindow);
+  lineBreak("Date:", String(date), lengthWindow);
+  console.log(underline);
 }
 
-function lineBreak(phraseInitial,phraseComplete, characters){
-    let phraseArray = phraseComplete.trim().split(' ')
-    let phrase = phraseInitial
-    for(let i = 0; i < phraseArray.length;i++){
-        let phraseTest = phrase
-        if ((phraseTest += ' ' +phraseArray[i]).length > characters-2){
-            console.log(`|${phrase.trim()}${' '.repeat(characters-phrase.trim().length-2)}|`)
-            phrase = phraseArray[i]
-        }else{phrase += ' ' +phraseArray[i]}
-
+function lineBreak(phraseInitial, phraseComplete, characters) {
+  let phraseArray = phraseComplete.trim().split(" ");
+  let phrase = phraseInitial;
+  for (let i = 0; i < phraseArray.length; i++) {
+    let phraseTest = phrase;
+    if ((phraseTest += " " + phraseArray[i]).length > characters - 2) {
+      console.log(
+        `|${phrase.trim()}${" ".repeat(characters - phrase.trim().length - 2)}|`
+      );
+      phrase = phraseArray[i];
+    } else {
+      phrase += " " + phraseArray[i];
     }
-    if(phrase !== undefined){console.log(`|${phrase.trim()}${' '.repeat(characters-phrase.trim().length-2)}|`)}
+  }
+  if (phrase !== undefined) {
+    console.log(
+      `|${phrase.trim()}${" ".repeat(characters - phrase.trim().length - 2)}|`
+    );
+  }
 }
 
+export function searchPost(posts) {
+  const option = options("searchPost");
+  let post = [];
+  switch (option) {
+    case 1:
+      let id = prompt("Digite o ID: ");
+      console.log("\n");
+      post = filterPosts("id", posts, id);
+      showPosts(post.id, post.title, post.content, post.date, 70);
+      break;
+    case 2:
+      let title = prompt("Digite o Title: ");
+      console.log("\n");
+      searchWitchArray(filterPosts("title", posts, title));
+      break;
+    case 3:
+      let content = prompt("Digite o Content: ");
+      console.log("\n");
+      searchWitchArray(filterPosts("content", posts, content));
+      break;
+    case 4:
+      let date = prompt("Digite o Date: ");
+      console.log("\n");
+      filterPosts("date", posts, date);
+      break;
+    case 5:
+      console.log("Voltando");
+      console.log("\n");
+      break;
+    default:
+      console.log("Erro... Voltando");
+      console.log("\n");
+  }
+}
+export function options(who) {
+  if (who === "searchPost") {
+    console.log(
+      `
+1- Buscar por ID;
+2- Buscar por Title;
+3- Buscar por Content;
+4- Buscar por Date;
+5- voltar.
+`
+    );
+  }
+  if (who === "main") {
+    console.log(
+      `
+1- Novo post;
+2- Ver posts;
+3- Procurar posts;
+4- Criar novo arquivos;
+5- voltar.
+`
+    );
+  }
+  let option = Number(prompt("Digite o número da opção desejada: "));
+  do {
+    if (!option || option < 1 || option > 5) {
+      console.log(option);
+      option = Number(prompt(`Opção inválida, escolha novamente:`));
+    }
+  } while (!option || option < 1 || option > 5);
+  return option;
+}
+
+function filterPosts(typeSearch, posts, dataSearch) {
+  let results = [];
+  switch (typeSearch) {
+    case "id":
+      if (posts[0] === undefined) {
+        if (posts.id === dataSearch) {
+          return posts;
+        }
+      } else {
+        for (let item of posts) {
+          if (item.id === dataSearch) {
+            return item;
+          }
+        }
+      }
+      break;
+    case "title":
+    if (posts[0] === undefined) {
+        if (posts.title === dataSearch) {
+          return posts;
+        }
+      } else {
+        posts.forEach((item) => {
+          if (item.title.includes(dataSearch)) {
+            results.push(item);
+          }
+        });
+        return results;
+      }
+
+      break;
+    case "content":
+      posts.forEach((item) => {
+        if (item.content.includes(dataSearch)) {
+          results.push(item);
+        }
+      });
+      return results;
+      break;
+    case "date":
+      console.log("Em breve... voltando.");
+      break;
+  }
+}
+function searchWitchArray(post) {
+  if (post[0] === undefined) {
+    showPosts(post.id, post.title, post.content, post.date, 70);
+  } else {
+    for (let item of post) {
+      showPosts(item.id, item.title, item.content, item.date, 70);
+    }
+  }
+}
